fb5032f164984e4a573a5c28b72408f2fabaa3b1
Dmytro Kataryna <dmytrokat@Dmytros-MacBook-Pro.local>
2022-06-27 13:24:52 +0300

    Added Hilt and Jetpack Nav components

--- app/src/main/java/kataryna/app/workmanager/ui/map/MapFragment.kt
+++ app/src/main/java/kataryna/app/workmanager/ui/map/MapFragment.kt
@@ -1,6 +1,9 @@
 package kataryna.app.workmanager.ui.map
 
 import androidx.fragment.app.Fragment
+import kataryna.app.workmanager.R
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(R.layout.fragment_map) {
+
+
 }
--- app/src/main/java/kataryna/app/workmanager/ui/tasks/TasksFragment.kt
+++ app/src/main/java/kataryna/app/workmanager/ui/tasks/TasksFragment.kt
@@ -4,28 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import kataryna.app.workmanager.R
 
-class TasksFragment : Fragment() {
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
-    companion object {
-        fun newInstance() = TasksFragment()
-    }
-
-    private lateinit var viewModel: TasksViewModel
+    private val viewModel: TasksViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
+    ) = inflater.inflate(R.layout.fragment_tasks, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[TasksViewModel::class.java]
+        val z = viewModel.hashCode()
+        view.findViewById<Button>(R.id.navigate_btn).setOnClickListener {
+            findNavController().navigate(R.id.action_tasksFragment_to_mapFragment)
+        }
     }
-
 }
